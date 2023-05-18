fbe034c65052b5da2c59720d724e5574bd157ccb
Xinghua Lu <xinghua@pitt.edu>
2023-05-17 22:45:55 -0400

    Bug fix

--- load_10X_matrices.py
+++ load_10X_matrices.py
@@ -11,7 +11,8 @@ import argparse
 import numpy as np
 import pandas as pd
 import scanpy as sc
-import AnnData as ad
+import anndata as ad
+
 
 def load_10X_matrices(matrix_dir):
     '''
@@ -26,30 +27,35 @@ def load_10X_matrices(matrix_dir):
         A single AnnData object containing the concatenated matrices.
 
     '''
+
+    #check if matrix_dir is a directory
+    if not os.path.isdir(matrix_dir):
+        print ("Input " + matrix_dir + " is not a directory")
+        return None
+    
     # Open the matrix directory and get the list of files
     matrix_files = os.listdir(matrix_dir)
 
-    # Initialize the AnnData object
-    adata = sc.AnnData()
-
     # Loop through the files and concatenate the matrices
     mtx_files = [x for x in matrix_files if '.mtx' in x]
+    if len(mtx_files) == 0:
+        print ("Input directory " + matrix_dir + " has no mtx files.\n")
+        return None
         
     # find out whether files has prefix, use set to remove duplicates
-    prefixes = set([x.split('matrix.mtx')[0] for x in mtx_files if '_matrix.mtx' in x]) 
+    prefixes = set([x.split('matrix.mtx')[0] for x in mtx_files if 'matrix.mtx' in x]) 
 
     if len(prefixes) > 0:
         # create a list to hold adatas
-        adata_list = []
-        matrix_dir_p = os.path.join(matrix_dir)
+        adata_list = []   
 
         # interate through prefixes and index mtx_files        
         for index, prefix in enumerate(prefixes): 
-            tmp = sc.read_10x_mtx(matrix_dir_p, prefix = prefix)
+            print("Loading " + prefix)
+            tmp = sc.read_10x_mtx(matrix_dir, prefix = prefix)
             tmp.obs['sample_ID'] = prefix 
             adata_list.append(tmp)
 
-
-    # concatenate adata_list
-    overall_adata = ad.concat(adata_list, join='outer')
-    return overall_adata
+        # concatenate adata_list
+        overall_adata = ad.concat(adata_list, join='outer')
+        return overall_adata
