730049f5442f427afd6d14fd7e714aaa83b54cb3
Xinghua Lu <xinghua@pitt.edu>
2023-05-20 17:56:52 -0400

    Add R script for transform Seurat to H5ad

--- SeuratObj2AnnDataH5ad.r
+++ SeuratObj2AnnDataH5ad.r
@@ -1,12 +1,20 @@
-# Install libraries if not avaliable
-# install.packages("Seurat")
-# if (!requireNamespace("remotes", quietly = TRUE)) {
-#   install.packages("remotes")
-# }
-# remotes::install_github("mojaveazure/seurat-disk")
+# This is an R script that reading a Seurat object and convert it to an AnnData file in h5ad format.
+
+# Usage: Rscript SeuratObj2AnnDataH5ad.r my_Seuratobj
+
 library(Seurat)
 library(SeuratDisk)
 
+# parse arguments
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) > 0) {
+  Seuratobj_file <- args[1]
+} else {
+  stop("No Seurat object provided")
+}
+
+# Load the Seurat object from disk
+my_Seuratobj <- readRDS(Seuratobj_file)
 # Convert the Seurat object to an AnnData file 
 SaveH5Seurat(my_Seuratobj, filename = "my_Seuratobj.h5Seurat")
 Convert("my_Seuratobj.h5Seurat", dest = "h5ad")
