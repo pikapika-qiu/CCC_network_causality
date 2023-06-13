443c3abe3605e29efafa0405c4cde86a9c46e459
Xinghua Lu <xinghua@pitt.edu>
2023-06-12 21:12:59 -0400

    Extraction of T cells are working

--- EGAS00001004809-rds2h5ad.r
+++ EGAS00001004809-rds2h5ad.r
@@ -10,7 +10,7 @@ library(Seurat)
 
 
 rdsfiles  = c('1863-counts_cells_cohort1.rds',   '1864-counts_tcell_cohort1.rds',  '1865-counts_myeloid_cohort1.rds',  '1866-counts_DC_cohort1.rds',  '1867-counts_cells_cohort2.rds')
-for (i in 1:length(rdsfiles)) {
+for (i in 1:seq_along(rdsfiles)) {
     rds_in = paste("/data/ICI_exprs/EGAS00001004809/", rdsfiles[i], sep="")
     print(paste("Reading RDS file:", rds_in))
     rds.obj <- readRDS(rds_in)
