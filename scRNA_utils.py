a52961b531c5bef2ec3eb2fb04a8448607e18546
Xinghua Lu <xinghua@pitt.edu>
2023-06-25 09:31:12 -0400

    Updates

---
+++ scRNA_utils.py
@@ -0,0 +1,136 @@
+''' 
+load_10X_matrices.py
+
+This function module will load a fold of 10X matrix files into a single sparse matrix
+and return a single AnnData object by concatenating the matrices.
+'''
+
+import os as os
+import sys as sys
+import argparse
+import numpy as np
+import pandas as pd
+import scanpy as sc
+import anndata as ad
+
+
+def load_10X_matrices(matrix_dir):
+    '''
+    load_10X_matrices(matrix_dir)
+    
+    This function will load a fold of 10X matrix files into a single sparse matrix.
+
+    Parameters:
+        matrix_dir (str): The directory containing the 10X matrix files.
+    
+    Returns:
+        A single AnnData object containing the concatenated matrices.
+
+    '''
+
+    #check if matrix_dir is a directory
+    if not os.path.isdir(matrix_dir):
+        print ("Input " + matrix_dir + " is not a directory")
+        return None
+    
+    # Open the matrix directory and get the list of files
+    matrix_files = os.listdir(matrix_dir)
+
+    # Loop through the files and concatenate the matrices
+    mtx_files = [x for x in matrix_files if '.mtx' in x]
+    if len(mtx_files) == 0:
+        print ("Input directory " + matrix_dir + " has no mtx files.\n")
+        return None
+        
+    # find out whether files has prefix, use set to remove duplicates
+    prefixes = set([x.split('matrix.mtx')[0] for x in mtx_files if 'matrix.mtx' in x]) 
+
+    if len(prefixes) > 0:
+        # create a list to hold adatas
+        adata_list = []   
+
+        # interate through prefixes and index mtx_files        
+        for index, prefix in enumerate(prefixes): 
+            print("Loading " + prefix)
+            tmp = sc.read_10x_mtx(matrix_dir, prefix = prefix, cache = True)
+            tmp.obs['sample_ID'] = prefix 
+            adata_list.append(tmp)
+
+        # concatenate adata_list
+        overall_adata = ad.concat(adata_list, join='outer')
+        return overall_adata
+    
+
+def labelClusterWithCellType(adata, cell_type_markers, cluster_column='leiden'):
+    '''
+    This function will label each cluster with the cell type that is most abundant in that cluster.
+
+    Parameters:
+        adata: AnnData object
+        cell_type_markers: a dictionary where the key is the cell type and the value is a list of markers for that cell type
+        cluster_column: the column in adata.obs that contains the cluster labels
+
+    Returns:
+        adata: AnnData object with a new column in adata.obs called 'cell_type' that contains the cell type label for each cell
+    
+    '''
+
+    # check if adata is AnnData object
+    if not isinstance(adata, ad.AnnData):
+        print ("Input adata is not an AnnData object")
+        return None
+    
+    # check if cell_type_markers is a dictionary
+    if not isinstance(cell_type_markers, dict):
+        print ("Input cell_type_markers is not a dictionary")
+        return None
+    
+    # Check if cell_type_markers is empty
+    if len(cell_type_markers) == 0:
+        print ("Input cell_type_markers is empty")
+        return None
+    
+    # Check if 'cluster_column' is in adata.obs
+    if cluster_column not in adata.obs.columns:
+        print ("Input cluster_column " + cluster_column + " is not in adata.obs")
+        return None
+    
+
+    # find total number of clusters   
+    cls_ids = adata.obs[cluster_column].unique()
+    
+    # iterate through all cluster
+    for i in cls_ids:
+        # find cells in cluster i        
+        cell_in_cls_i = adata.obs[cluster_column] == i  
+        # this will return a vector of True/False where True means the cell is in cluster i
+        # print('processing cluster: ' + str(i) + ' with ' + str(sum(cell_in_cls_i)) + ' cells')
+
+        # keep track of which cell type is most abundant in cluster i
+        cell_type_cluster_overlapp_pct = dict()
+
+        #iterate through key and value of cell_type_markers
+        for cell_type, marker_genes in cell_type_markers.items():   
+            # Extract the expression of all marker genes for cells in cluster i
+            # this will return a sparse matrix of cells x markers
+            cell_w_marker_genes = adata.raw.X[:, adata.raw.var_names.isin(marker_genes)] > 0  
+            
+            # change cell_in_cls_i to numpy array and repeat it to match the shape of cell_w_marker_genes
+            cell_in_cls_i_m = np.tile(cell_in_cls_i.to_numpy(), (cell_w_marker_genes.shape[1], 1)).T
+
+            # find cells in cluster i that express the marker
+            # this create a matrix of cells x markers where True means the cell express the marker and in cluster i
+            cell_w_marker_genes = cell_w_marker_genes.toarray() & cell_in_cls_i_m
+
+            # caclualte average markers expressed in each cell in Marker_genes_i
+            nmarker_per_cell = np.sum(cell_w_marker_genes, axis=0) / cell_w_marker_genes.shape[1]
+            #print(nmarker_per_cell)
+
+            # keep track of which cell type is most abundant in cluster i
+            # assuming the cell type with the highest average marker present is the most abundant         
+            cell_type_cluster_overlapp_pct[cell_type] = np.sum(nmarker_per_cell) / sum(cell_in_cls_i)
+
+        # check with cell type is most abundant in cluster i
+        max_type = max(cell_type_cluster_overlapp_pct, key=cell_type_cluster_overlapp_pct.get)
+        print('Cluster ' + str(i) + ' is most likely ' + max_type + ' with ' + str(cell_type_cluster_overlapp_pct[max_type]) + ' overlap')
+        adata.obs.loc[cell_in_cls_i, 'cell_type'] = max_type    
